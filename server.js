e22724cf2c350443aeb9556155bdf09deab9c24c
Run <run2gotv@gmail.com>
2024-02-15 12:47:45 +0100

    established socket.io base, added JS injection to client

--- server.js
+++ server.js
@@ -15,83 +15,73 @@ HELP      Print this message.`;
 const fs = require('fs-extra'); // Use file system operations
 const path = require('path'); // Allows working with file & directory paths
 const console = require('./log.js'); // Use the logging functionality inside the log.js file
-const express = require('express'); // Make use of the express.js framework for the core application
-const app = express();
-const http = require('http').Server(app); // Use express app with http server
-const io = require('socket.io')(http); // Integrate socket.io with http server
+var express = require('express'), // Make use of the express.js framework for the core application
+    app = express(), // Use express app with http server
+    http = require('http'), // Require http module
+    socketIO = require('socket.io'),  // Integrate socket.io
+    server, io;
 
 let program; // Declare process variable
 
 async function serverRun() {
 	try {
-		async function copySocketFolder(){
-			const srcPath = path.join(__dirname, 'node_modules/socket.io/client-dist/socket.io.min.js');
-			const destPath = path.join(__dirname, 'web/inc/socket.io/socket.io.min.js');
-			await fs.ensureDir(path.dirname(destPath));
-			await fs.copyFile(srcPath, destPath);
-			//const srcPath = path.join(__dirname, 'node_modules/socket.io/client-dist/');
-			//const destPath = path.join(__dirname, 'web/inc/socket.io/');
-			//// Copy the entire folder
-			//await fs.copy(srcPath, destPath, {
-			//	recursive: true,
-			//	overwrite: true
-			//}, fs.constants.COPYFILE_EXCL);
-		}
-		await copySocketFolder();
-
-        // Integrate socket.io middleware into Express.js app
-		const socket = io.of('/socket.io'); // Namespace declaration for socket.io communication
-		socket.on('connection', (socket) => {
-			console.log('Client connected');
-
-			socket.on('input', (data) => {
-				console.log('Received input from client:', data);
-				// Process data as needed
-				// For testing, let's just echo it back to the client
-				socket.emit('output', `Server received input: ${data}`);
-			});
-
-			// You can handle other events here if needed
-			socket.on('disconnect', () => {
-				console.log('Client disconnected');
-			});
-		});
-
-
+		await copySocketFile();
+		
         // Serve static files from the 'web' directory
         app.use(express.static(path.join(__dirname, 'web')));
-
-		function printRequest(req) {
-			console.debug(`Request:\nHeader:\n${JSON.stringify(req.headers)}\nParams:\n${JSON.stringify(req.params)}\nBody:\n${JSON.stringify(req.body)}`);
-		}
-		let isAgentCLI = false;
-		app.get("*", (request, response) => { // Handle GET requests
+		
+		app.get('*', function (request, response) { // Handle GET requests
+			//response.sendFile(__dirname + '/web/index.html');
 			try {
 				printRequest(request);
-				isAgentCLI = detectAgent(request.headers['user-agent'].toLowerCase())
-				//serveData(response, isAgentCLI);
+				isAgentCLI = detectAgent(request.headers['user-agent'].toLowerCase());
+				serveData(response, isAgentCLI);
 				response.sendFile(path.join(__dirname, './web/index.html'));
 			} catch (error) { console.error(error.message); }
 		});
-		app.post("*", (request, response) => { // Handle post requests
+		app.post("/", (request, response) => { // Handle post requests
 			try {
 				printRequest(request);
 				receiveData(request);
 			} catch (error) { console.error(error.message); }
 		});
+			
+		server = http.Server(app);
+		io = socketIO(server);
+		//server.listen(3000);
 		
+		io.on('connection', function (socket) {
+			console.log('Client connected');
+			socket.on('cmd', function (data) {
+				console.log('Input:', data);
+				const commands = data.split(" ");
+				if (commands[0] === 'alert') {
+					socket.emit('execute', `alert("${commands[1]}");`);
+				}
+				else if (commands[0] == 'login') {
+					console.log('Login attempt with', commands[1]);
+				}
+				//socket.emit('output', commands); // Echo cmd back
+			});
+
+			// You can handle other events here if needed
+			socket.on('disconnect', () => {
+				// Start counter
+				setTimeout(() => { containerShutdown(); }, 1200000); // => 20min
+				console.log('Client disconnected');
+			});
+		});
+
+		function printRequest(req) {
+			console.debug(`Request:\nHeader:\n${JSON.stringify(req.headers)}\nParams:\n${JSON.stringify(req.params)}\nBody:\n${JSON.stringify(req.body)}`);
+		}	
 		function detectAgent(user) {
 			console.debug(user);
 			return !!(user.includes('curl') || user.includes('shell'));
 		}
-
 		function serveData(response, isAgentCLI) {
 			console.debug("CLI Agent: " + isAgentCLI);
-			if (isAgentCLI) {
-				response.sendFile(path.join(__dirname, './cli.sh'));
-			} else {
-				response.sendFile(path.join(__dirname, './web/index.html'));
-			}
+			response.sendFile(path.join(__dirname, isAgentCLI ? './cli.sh' : './web/index.html'));
 		}
 		function receiveData(request) {
 			// Placeholder
@@ -109,14 +99,27 @@ async function serverRun() {
 			}
 		});
 		
-		console.log(`${serverName} started`); // Notify that the server has been started
-		program = app.listen(serverPort, () => { console.log(`Now listening on port ${serverAddress}:${serverPort}`); }); // Bind the server to the specified port
+		console.log(`${serverName} WebApp started`); // Notify that the server has been started
+		program = server.listen(serverPort, () => { // Bind the server to the specified port
+			console.log(`Now listening on ${serverAddress}:${serverPort}`);
+		});
 	} catch (error) { console.error(`[ERROR] ${error.message}`); }
 }
 
-process.on('SIGTERM', serverTerminate); // Handle shutdown signals
-process.on('SIGQUIT', serverShutdown);
-process.on('SIGINT', serverShutdown);
+async function copySocketFile(){
+	const srcPath = path.join(__dirname, 'node_modules/socket.io/client-dist/socket.io.min.js');
+	const destPath = path.join(__dirname, 'web/inc/socket.io/socket.io.min.js');
+	await fs.ensureDir(path.dirname(destPath));
+	await fs.copyFile(srcPath, destPath);
+}
+
+function containerStart(){
+	// Start container
+}
+
+function containerShutdown() {
+	// Shut container
+}
 
 function serverRestart() {
 	program.close(() => { console.log(`${serverName} restarted`); }); // Close the server, trigger restart
@@ -124,11 +127,15 @@ function serverRestart() {
 function serverShutdown() { // Graceful shutdown function, forces shutdown if exit process fails
 	console.log(`${serverName} stopped`);
     program.close(() => { process.exit(0); });
-    setTimeout(() => { serverTerminate(); }, 2000); // Force shutdown if server hasn't stopped within 2s
+    setTimeout(() => { serverTerminate(); }, 100); // Force shutdown if server hasn't stopped within 0.1s
 }
 function serverTerminate() {
 	console.error(`${serverName} terminated`);
 	process.exit(12);
 }
+
+process.on('SIGTERM', serverTerminate); // Handle shutdown signals
+process.on('SIGQUIT', serverShutdown);
+process.on('SIGINT', serverShutdown);
 
 serverRun(); // Start the async server function
