756fd621cf7ff4f0b0f92c1ae89ba67dbea4be68
run2go <run2gotv@gmail.com>
2024-02-18 20:44:38 +0100

    implemented logic to grep tunnel URL

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -6,8 +6,23 @@
 # Append a line break to motd
 echo "" >> /etc/motd
 
-# Execute cloudflared and extract the domain, then append to motd
-echo "Temporary URL to port 80: $(/usr/local/bin/cloudflared | grep -oh "https://\(.*\)trycloudflare.com")" >> /etc/motd
+# Start cloudflared service in the background
+/usr/local/bin/cloudflared cloudflared tunnel --no-autoupdate --url http://localhost:80 &
+
+# Wait for cloudflared to output the trycloudflare.com subdomain and append it to motd
+while true; do
+    # Get the process ID (PID) of the cloudflared process
+    PID=$(pgrep -f "cloudflared tunnel --no-autoupdate --url http://localhost:80")
+    if [ -n "$PID" ]; then
+        # Extract the subdomain from the cloudflared command line arguments
+        DOMAIN=$(cat /proc/$PID/cmdline | tr '\0' '\n' | grep -oh "https://\(.*\)trycloudflare.com")
+        if [ -n "$DOMAIN" ]; then
+            echo "Quick tunnel accessible via: $DOMAIN" >> /etc/motd
+            break
+        fi
+    fi
+    sleep 1
+done
 
 # Execute the commands passed to the container
 exec "$@"
