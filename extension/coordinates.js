4fdc8a7b435576a3a680441c5ca64c81ba4f38be
Run <no@mail.com>
2024-02-21 21:03:27 +0100

    adjusted handle() to update screen upon change

--- extension/coordinates.js
+++ extension/coordinates.js
@@ -6,21 +6,27 @@ const fs = require('fs-extra');
 
 // Handle various screen coordinates, trigger keyboard
 function handle(socket, sessionId, coords) {
-    let [posX, posY] = coords;
-    let [screenWidth, screenHeight] = session.read(sessionId, ['screenWidth', 'screenHeight']);
-
-    console.log(`aa + ${screenWidth}`);
-    
-    let widthCenter = screenWidth / 2;
-    let heightCenter = screenHeight / 2;
-    if ( (posX < widthCenter * 1.2) && (posX > widthCenter * 0.8) && (posY < heightCenter * 1.2) && (posY > heightCenter * 0.8) ) {
-        const keyboardScript = fs.readFileSync('./stream/keyboard.js', 'utf8');
-        socket.emit('eval', keyboardScript);
+    const [posX, posY, width, height] = coords; // Deconstruct coords array
+    const {screenWidth, screenHeight, terminalId} = session.read(sessionId, ['screenWidth', 'screenHeight', 'terminalId']); // Extract stored screen size
+    if (width != screenWidth || height != screenHeight) { // Update screen size if changed
+        session.update(sessionId, {screenWidth: width, screenHeight: height}); 
     }
-    let widthTopRight = screenWidth;
-    let heightTopRight = screenHeight;
-    if ( (posX > widthTopRight * 0.9) && (posY < heightTopRight * 0.1) ) {
-        cmd.reset(socket, sessionId);
+
+    if (!terminalId) {
+        // Handle interactions near the center
+        const widthCenter = screenWidth / 2;
+        const heightCenter = screenHeight / 2;
+        if ((posX < widthCenter * 1.2) && (posX > widthCenter * 0.8) && (posY < heightCenter * 1.2) && (posY > heightCenter * 0.8) ) {
+            const keyboardScript = fs.readFileSync('./stream/keyboard.js', 'utf8');
+            socket.emit('eval', keyboardScript);
+        }
+
+        // Handle exit/close interactions
+        const widthTopRight = screenWidth;
+        const heightTopRight = screenHeight;
+        if ((posX > widthTopRight * 0.9) && (posY < heightTopRight * 0.1) ) {
+            cmd.reset(socket, sessionId);
+        }
     }
 }
 
