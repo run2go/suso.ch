c21fab5e48f0c3343dfcc47f336eca4d951bb9c2
run2go <run2gotv@gmail.com>
2024-03-01 15:57:58 +0100

    trimmed obsolete docker start

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ sudo wg-quick up /etc/wireguard/wg0.conf
 sudo touch /etc/sysctl.conf
 sudo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
 
-# Start docker application
-node /usr/src/app/server.js
-
 # Route all outgoing docker traffic through the wireguard interface, or not at all
 sudo iptables -A FORWARD -i wg0 -o docker0 -j ACCEPT
 sudo iptables -A FORWARD -i docker0 -o wg0 -j ACCEPT
